2317e0e851d672d9ce6110e52a4b79b17576ca69
Justas Sabeckis <justas.sabeckis@gmail.com>
2026-02-09 15:40:31 +0200

    Pradinis

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using std::left;
+using std::setw;
