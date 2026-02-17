2e4188cde2739c66399d2b77049bccea9bcea222
ICYBATT <91900020+ICYBATT@users.noreply.github.com>
2026-02-17 22:28:25 +0200

    Mediana

--- main.cpp
+++ main.cpp
@@ -22,3 +22,14 @@ struct Studentas {
     double rez;
 };
 
+double mediana(vector<int> paz) {
+    if (paz.empty()) return 0.0;
+    std::sort(paz.begin(), paz.end());
+    int n = paz.size();
+
+    if (n % 2 == 1)
+        return paz[n / 2];
+    else
+        return (paz[n / 2 - 1] + paz[n / 2]) / 2.0;
+}
+
