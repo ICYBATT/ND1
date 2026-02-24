9221e747ac001997a1ca1956a8a2b61132b1b770
ICYBATT <91900020+ICYBATT@users.noreply.github.com>
2026-02-24 23:50:23 +0200

    Mediana ir random pazimys

--- main.cpp
+++ main.cpp
@@ -29,11 +29,22 @@ struct Studentas {
     double gal_med = 0.0;
 };
 
+double mediana(const vector<int> &paz) {
+    if (paz.empty()) return 0.0;
+    vector<int> tmp = paz;
+    std::sort(tmp.begin(), tmp.end());
+    int n = (int)tmp.size();
+    if (n % 2 == 1) return (double)tmp[n / 2];
+    return (tmp[n / 2 - 1] + tmp[n / 2]) / 2.0;
+}
+
+int atsitiktinis_pazymys() {
+    return rand() % 10 + 1;
+}
+
 int main() {
-    
+    srand((unsigned)time(NULL));
     Studentas s;
-    cout << "Iveskite varda ir pavarde (J S): ";
-    cin >> s.vardas >> s.pavarde;
-    cout << "Ivesti pavyzdinis studentas: " << s.vardas << " " << s.pavarde << "\n";
+    cout << "Mediana pavyzdys, atsitiktinis pazymys: " << atsitiktinis_pazymys() << "\n";
     return 0;
 }
