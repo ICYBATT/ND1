9eaa2546a8d1e38710f4b069bd94bbcae43a2fe2
ICYBATT <91900020+ICYBATT@users.noreply.github.com>
2026-03-05 20:31:33 +0200

    studentas

---
+++ studentas.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> paz;
+    int egz = 0;
+    double gal_vid = 0.0;
+    double gal_med = 0.0;
+};
+
+double mediana(const std::vector<int>& paz);
+void skaiciuoti(Studentas& a);
+int atsitiktinis_pazymys();
