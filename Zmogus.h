2ddaaf6405f16984bac4ef4012b86d1b656a0fc9
ICYBATT <91900020+ICYBATT@users.noreply.github.com>
2026-05-07 17:36:26 +0300

    Dokumentuoti Zmogus klase

--- Zmogus.h
+++ Zmogus.h
@@ -1,23 +1,66 @@
 #pragma once
 #include <string>
 
+/**
+ * @class Zmogus
+ * @brief Abstrakti bazinė žmogaus klasė.
+ *
+ * Ši klasė saugo pagrindinius žmogaus duomenis:
+ * vardą ir pavardę.
+ *
+ * Klasė naudojama kaip bazinė klasė Studentas klasei.
+ */
 class Zmogus {
 protected:
     std::string vardas_;
     std::string pavarde_;
 
 public:
+
+    /**
+     * @brief Default konstruktorius.
+     */
     Zmogus();
+
+    /**
+     * @brief Parametrinis konstruktorius.
+     * @param vardas Žmogaus vardas.
+     * @param pavarde Žmogaus pavardė.
+     */
     Zmogus(const std::string& vardas, const std::string& pavarde);
 
+    /**
+     * @brief Virtualus destruktorius.
+     */
     virtual ~Zmogus() = 0;
 
+    /**
+     * @brief Išveda žmogaus informaciją.
+     */
     virtual void spausdintiInformacija() const = 0;
 
+    /**
+     * @brief Grąžina vardą.
+     * @return Žmogaus vardas.
+     */
     const std::string& getVardas() const;
+
+    /**
+     * @brief Grąžina pavardę.
+     * @return Žmogaus pavardė.
+     */
     const std::string& getPavarde() const;
 
+    /**
+     * @brief Nustato vardą.
+     * @param vardas Naujas vardas.
+     */
     void setVardas(const std::string& vardas);
+
+    /**
+     * @brief Nustato pavardę.
+     * @param pavarde Nauja pavardė.
+     */
     void setPavarde(const std::string& pavarde);
 };
 
